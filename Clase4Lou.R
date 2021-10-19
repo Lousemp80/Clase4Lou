680b9e3b79a72e34f9758d28de3c3851bad20d33
Lousemp80 <lourdes.sempert@gmail.com>
2021-10-19 01:40:40 -0300

    mas practica

---
+++ Clase4Lou.R
@@ -0,0 +1,7 @@
+library(electorAr)
+library(tidyverse)
+show_available_elections(source = "data")
+library(geoAr)
+library(tibble)
+show(tribble(source("data")))
+?get_names
---
+++ practicandoclase1.R
@@ -0,0 +1,28 @@
+### Practicando clase 1
+
+2+2
+suma <- 2+2
+suma
+suma <- 3+4
+suma
+suma <- "3+4"
+class(suma)
+
+suma_numerica <- 6+6
+class(suma_munerica)
+suma_numerica
+class(suma_numerica)
+
+suma_logica <- is.character(suma_numerica)
+is.character(suma_numerica)
+class(suma_logica)
+
+c(1, 2, 3, 4)
+c(1:4)
+
+datos <- c(1:4)
+
+class(datos)
+
+otros <- c(17:54)
+otros
